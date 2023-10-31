1e454d07a3c1afb8da40aec661b1e5f5fa4f6893
Vera Smulskaya <vera.smulskaya@gmail.com>
2023-10-31 21:23:13 +0200

    add onSubmit to form component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import Title from '../Title/Title';
 import css from './ContactForm.module.css';
+import { computeHeadingLevel } from '@testing-library/react';
 
 export default class ContactForm extends Component {
+handleSubmit = event => {
+  event.preventDefault();
+  const name = event.currentTarget.elements.name.value;
+  const number = event.currentTarget.elements.number.value;
+
+  console.log("Form submit", name, number);
+}
+
   render() {
     return (
       <div className={css.formContainer}>
         <Title>Contact Form</Title>
-        <form className={css.form}>
+        <form onSubmit={this.handleSubmit} className={css.form}>
           <label className={css.formLabel}>
             <p  className={css.formLabelText}>Name: </p>
-            <input type="text" className={css.formInput}></input>
+            <input type="text" className={css.formInput} name='name'></input>
           </label>
           <label>
             <p className={css.formLabelText}>Number: </p>
-            <input type="number"  className={css.formInput}></input>
+            <input type="number"  className={css.formInput} name='number'></input>
           </label>
           <button type="button" className={css.formButton}>Add contact</button>
         </form>
-      </div>
+       </div>
     );
   }
 }
